cc2e65af98a6192c4dff92d10c135ff6499dcf9b
ola9999 <ola.ms84.99@gmail.com>
2023-07-15 14:28:02 +0300

    update requirements

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@
 set -o errexit
 /opt/render/project/src/.venv/bin/python -m pip install -r requirements.txt
 
-poetry install
-poetry lock
+# poetry install
+# poetry lock
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
 /opt/render/project/src/.venv/bin/python manage.py collectstatic --no-input
 /opt/render/project/src/.venv/bin/python manage.py migrate
 
-poetry add gunicorn
+# poetry add gunicorn
